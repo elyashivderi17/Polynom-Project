abe64616bef05fc6bf18119eb2e49166b257dbbb
elyashiv <elyashivderi17@gmail.com>
2018-11-13 12:31:30 +0200

    deleat testmonon

--- gral/gral-core/src/main/java/de/erichseifert/gral/package-info.java
+++ gral/gral-core/src/main/java/de/erichseifert/gral/package-info.java
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with GRAL.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
+/**/////
  * Main classes.
  */
 package de.erichseifert.gral;
